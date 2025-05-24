73b51dcca24fd591d4b9653834db9bb8c1d4af25
birdex <sameerpesari@gmail.com>
2025-05-24 01:21:15 -0700

    updated compile script

--- cne.sh
+++ cne.sh
@@ -11,24 +11,35 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+> err
+
 if [ "$1" == 0 ]; then
-    g++ src/tetris.cpp src/functions.cpp -o dev -lncurses && 
-    rm bin/dev ;
+    g++ src/tetris.cpp src/functions.cpp -o dev -lncurses &&
+    rm -f bin/dev ;
     mv dev bin/ &&
-    gnome-terminal -- ./bin/dev
+    gnome-terminal --wait -- bash -c './bin/dev; echo $? > err'
+    echo "Ran: ./bin/dev"
 elif [ "$1" == 1 ]; then
     g++ -O1 src/tetris.cpp src/functions.cpp -o stable -lncurses &&
-    rm bin/stable ;
+    rm -f bin/stable ;
     mv stable bin/ && 
-    gnome-terminal -- ./bin/stable
+    gnome-terminal --wait -- bash -c './bin/stable; echo $? > err'
+    echo "Ran: ./bin/stable"
 elif [ "$1" == 2 ]; then
     g++ -O3 src/tetris.cpp src/functions.cpp -o perf -lncurses &&
-    rm bin/perf ;
+    rm -f bin/perf ;
     mv perf bin/ &&
-    gnome-terminal -- ./bin/perf
+    gnome-terminal --wait -- bash -c './bin/perf; echo $? > err'
+    echo "Ran: ./bin/perf"
 else
     echo "Wrong compilation type"
     exit 2
 fi
+
+if [ "$(cat err | tr -d '\n')" = "139" ]; then
+    echo "Segmentation Fault (core dumped)"
+    exit 3
+fi
+
 
 exit 0
